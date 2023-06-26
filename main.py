bb846d3151df46f36b14bb86ae8bbbe6a9e4ce7e
Pluto <darlann.bertolonelopez@gmail.com>
2023-06-26 11:38:18 +0200

    DO comparaison first

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 
 r=6371*1000 #Earth radius [m]
+DOinit:float
 class Avion():
     
     heading:float #[degree]
@@ -10,15 +11,16 @@ class Avion():
     longitude:float #[degree] or Theta
     altitude:float #[ft]
     global r
-    rho= r+altitude #Earth radius [m] + Altitude
-
-    def __init__(self, heading:float,horizontalSpeed:float,verticalSpeed:float, latitude:float,longitude:float,altitude:longitude):
+    
+    def __init__(self, heading:float,horizontalSpeed:float,verticalSpeed:float, latitude:float,longitude:float,altitude:float):
         self.heading = heading
         self.horizontalSpeed = horizontalSpeed
         self.verticalSpeed = verticalSpeed
         self.latitude=latitude
         self.longitude=longitude
         self.altitude=altitude
+        rho= r+altitude #Earth radius [m] + Altitude
+
 
     def deplacement(heading, horizontalSpeed, verticalSpeed, latitude, longitude, altitude):
         i=0 #test time in second
@@ -55,15 +57,48 @@ class Conversion():
     ## UNITY CONVERSION TO INTERNATIONAL SYSTEM ##
     DEG2RAD= math.pi/180 #Conversion degree to rad
     KT2MS= 1852/3600 #Conversion kt to m/s
-    FT2M=0,3048 #Conversion ft to meters
-    FTMIN2MS=FT2M/60 #Conversion ft/min to m/s
+    FT2M:float=0,3048 #Conversion ft to meters
+    FTMIN2MS=0,3048*1/60 #Conversion ft/min to m/s
 
-def convergence():
+def distanceOblique():
     global r
     phiA:float #latitude Avion 1
     phiB:float #latitude Avion 2
     thetaA:float #longitude Avion 1
     thetaB:float #longitude Avion 2
 
-    DO=r*math.acos(math.cos(thetaA)*math.cos(thetaB)+math.sin(thetaA)*math.sin(thetaB)*math.cos(phiB-phiA)) #Oblical distance formula from the ground projected points
+    phiA = chasseur.latitude
+    phiB = bandit.latitude
+    thetaA = chasseur.longitude
+    thetaB = chasseur.longitude
+
+    DOinit=r*math.acos(math.cos(thetaA)*math.cos(thetaB)+math.sin(thetaA)*math.sin(thetaB)*math.cos(phiB-phiA)) #Oblical distance formula from the ground projected points
+    print(DOinit)
+    return DOinit
+
     
+def comparaison():
+
+    global DOinit
+    phiAfin = chasseur.latitude
+    phiBfin = bandit.latitude
+    thetaAfin = chasseur.longitude
+    thetaBfin = chasseur.longitude
+
+    DOfin=r*math.acos(math.cos(thetaAfin)*math.cos(thetaBfin)+math.sin(thetaAfin)*math.sin(thetaBfin)*math.cos(phiBfin-phiAfin))
+    print (DOfin)
+
+    if DOinit<DOfin:
+        print("Divergence")
+    elif DOinit>DOfin:
+        print("Convergence")
+    elif DOinit==DOfin:
+        print("Ni convergence ni divergence")
+
+chasseur = Avion(0,100,0,45,0,5000)
+bandit = Avion(90,100,0,45,0,5000)
+
+distanceOblique()
+chasseur.deplacement
+bandit.deplacement
+comparaison()
